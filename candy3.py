aee9598be3aa1ae244831fce59c916060a4c7033
Bhanu P Singh <bhanu.s@directi.com>
2012-09-30 18:24:05 +0530

    Added candy3.py and quadarea.cpp

---
+++ candy3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+def main():
+  t = int(raw_input())
+  while t:
+    t -= 1
+    s = raw_input()
+    n = int(raw_input())
+    sum = 0
+    for i in range(n):
+      sum = (sum + int(raw_input())) % n
+    if sum % n == 0:
+      print 'YES'
+    else:
+      print 'NO'
+      
+if __name__ == '__main__':
+  main()       
