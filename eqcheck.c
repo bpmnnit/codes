b8cf59298fb251a5850759bf6a365250b74d4267
Bhanu P Singh <bhanu.s@directi.com>
2012-10-16 19:29:55 +0530

    Added EQCHECK challenge problem of spoj

---
+++ eqcheck.c
@@ -0,0 +1,3 @@
+#include<math.h>
+gcd(int m,int n){if(n==0)return m;else return gcd(n,m%n);}main(){int a,b,c,t,g;char x,y,e,p;scanf("%d",&t);while(t--){scanf("%d%c%c%d%c%c%d",&a,&x,&p,&b,&y,&e,&c);g=gcd(abs(a),abs(b));if(c%g==0)printf("Yes\n");else printf("No\n");}}
+//EQCHECK problem of SPoj. Logic: ax+by=c will have infinite lattice points if c is a multiple of gcd(a, b)
