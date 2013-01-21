4f3898797d9ab4bb770112fa7bac22f49ae04b86
Bhanu P Singh <bhanu.s@directi.com>
2013-01-21 18:35:26 +0530

    added a sample program to find the number of divisors of a number efficiently

---
+++ num_divisors.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+def main():
+  ''' This python program returns the number of divisors of an input number '''
+  n = int(raw_input())
+  initial_n = n
+  num_factors = 1
+  i = 2
+  while i * i <= initial_n:
+    power = 0
+    while n % i == 0:
+      n /= i
+      power += 1
+    num_factors *= (power + 1)
+    if n <= 1:
+      break
+    i += 1
+  if n > 1:
+    num_factors *= 2
+  print num_factors
+    
+if __name__ == '__main__':
+  main()      
